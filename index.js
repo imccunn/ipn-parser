187fe9cd703fa38a97ab2edaa1d597342fe8b861
Ian McCunn <ianmccunn@gmail.com>
2015-11-22 13:27:10 -0800

    init

---
+++ index.js
@@ -0,0 +1,111 @@
+#!/usr/bin/env node
+
+var tones = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0];
+var newSet = [];
+
+var PC = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab'];
+var CSet = PCShift('C');
+/**
+ * Returns a PC array whose first element is the passed PC.
+ */
+function PCShift(rootPitch) {
+  var re;
+  if (rootPitch.length === 1) re = new RegExp(rootPitch + "(?!#|b)", "g");
+  else re = new RegExp(rootPitch, "g");
+  var newPCSet = PC.slice();
+  for (var i = 0; i < PC.length; i++) {
+    var check = re.exec(PC[i]);
+    if (check) {
+      return newPCSet;
+    }
+    newPCSet.push(newPCSet.shift());   
+  }
+  return newPCSet;
+}
+
+function setIndex(pc, set) {
+  var cRe = pc.length === 2 ? pc : pc + '(?!#|b)';
+  var re = new RegExp(cRe, 'g');
+  for (var i = 0; i < set.length; i++) {
+    if (re.exec(set[i])) return i;
+  }
+  return -1;
+}
+
+const BASE_FREQ = 440; // A4
+const C4 = 261.626; // Middle C
+
+var intervals = {
+  m2: 1,
+  M2: 2,
+  m3: 3,
+  M3: 4,
+  P4: 5,
+  TT: 6,
+  P5: 7,
+  m6: 8,
+  M6: 9,
+  m7: 10,
+  M7: 11
+};
+
+var enharmonics = {
+};
+
+var qualities = {
+  maj: [0, 4, 7],
+  min: [0, 3, 7],
+  dim: [0, 3, 6]
+};
+
+function rand() {
+  return Math.random();
+}
+
+function randM(min, max) {
+  return Math.floor(rand() * (max-min) + min);
+}
+
+function freqOfPitchFromA(stepsFrom440) {
+  return BASE_FREQ * Math.pow(Math.pow(2, (1/12)), stepsFrom440);
+}
+
+function stepFromBaseFreq() {}
+
+// SPN (Scientific Pitch Notation)
+function SPNtoFreq(spn) {
+  var stepsFrom440 = pcDiff('A4', spn);
+  return freqOfPitchFromA(stepsFrom440);
+}
+
+
+function pcDiff(pc1, pc2) {
+
+  if (pc1.length > 3 || pc1.length === 0 ||
+      pc2.length > 3 || pc2.length === 0) {
+    return 'bad input';
+  }
+
+  var pc1 = pc1.split('');
+  var pc2 = pc2.split('');
+
+  pc1 = pc1.length === 2 ? {pc: pc1[0], o: parseInt(pc1[1])} : {pc: pc1[0] + pc1[1], o: parseInt(pc1[2])};
+  pc2 = pc2.length === 2 ? {pc: pc2[0], o: parseInt(pc2[1])} : {pc: pc2[0] + pc2[1], o: parseInt(pc2[2])};
+  
+  var octDiff = pc1.o - pc2.o;
+  pc1.cIndex = setIndex(pc1.pc, CSet);
+  pc2.cIndex = setIndex(pc2.pc, CSet);
+  var ic = pc1.cIndex - pc2.cIndex;
+
+  return octDiff === 0 ? ic : (octDiff * 11) + ic;
+}
+
+console.log(pcDiff('C4', 'D#6'));
+console.log(pcDiff('C#4', 'G#4'));
+console.log(pcDiff('Bb3', 'F3'));
+console.log(pcDiff('B5', 'B5'));
+console.log(pcDiff('A2', 'A2'));
+console.log(SPNtoFreq('A3'));
+console.log(SPNtoFreq('A4'));
+console.log(SPNtoFreq('A5'));
+
