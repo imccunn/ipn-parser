ce70c44daf6e84b6cba9bbd815afb76fc5ddcd7d
Ian McCunn <ianmccunn@gmail.com>
2017-02-24 21:14:38 -0800

    Adds all tests for indices of all pitches in set.

--- test/index.js
+++ test/index.js
@@ -33,7 +33,42 @@ describe('setIndex() test', () => {
     expect(setIndex('Db', set)).to.eql(4);
   });
 
-  it('given bad input, should return -1', () => {
+  it('should return 5 given input D', () => {
+    expect(setIndex('D', set)).to.eql(5);
+  });
+
+  it('should return 6 given input D# or Eb', () => {
+    expect(setIndex('D#', set)).to.eql(6);
+    expect(setIndex('Eb', set)).to.eql(6);
+  });
+
+  it('should return 7 given input E', () => {
+    expect(setIndex('E', set)).to.eql(7);
+  });
+
+  it('should return 8 given input F', () => {
+    expect(setIndex('F', set)).to.eql(8);
+  });
+
+  it('should return 9 given input F# Gb', () => {
+    expect(setIndex('F#', set)).to.eql(9);
+    expect(setIndex('Gb', set)).to.eql(9);
+  });
+
+  it('should return 10 given input G', () => {
+    expect(setIndex('G', set)).to.eql(10);
+  });
+
+  it('should return 11 given input G# or Ab', () => {
+    expect(setIndex('G#', set)).to.eql(11);
+    expect(setIndex('Ab', set)).to.eql(11);
+  });
+
+  it('should return -1 for bad input `Z`', () => {
     expect(setIndex('Z', set)).to.eql(-1);
-  })
-})
+  });
+
+  it('should return -1 for bad input `123`', () => {
+    expect(setIndex(123, set)).to.eql(-1);
+  });
+});
