8eb877bd666240d6268a37407ed64b5c24881470
Ian McCunn <ianmccunn@gmail.com>
2017-02-26 17:09:35 -0800

    Add test for multi-octave pcDiff inputs
    - Adds longer description for test suite.

--- test/pcDiff.test.js
+++ test/pcDiff.test.js
@@ -2,12 +2,16 @@ const pcDiff = require('../lib/pcDiff');
 const chai = require('chai');
 const expect = chai.expect;
 
-describe('pcDiff specification - ', () => {
+let text = 'pcDiff() specification - \n\t' +
+  '- Should return the intervallic difference as a positive or negative integer,\n\t' +
+  '  indicating steps, between the two passed in pitches.';
+
+describe(text, () => {
   it('should return a difference of 5 with input C4 and F4', () => {
     expect(pcDiff('C4', 'F4')).to.eql(5);
   });
 
-  it('should return a difference of 5 with input C4 and F3', () => {
+  it('should return a difference of -7 with input C4 and F3', () => {
     expect(pcDiff('C4', 'F3')).to.eql(-7);
   });
 
@@ -29,6 +33,11 @@ describe('pcDiff specification - ', () => {
 
   it('should return a difference of -19 with input F#5 and B3', () => {
     expect(pcDiff('F#5', 'B3')).to.eql(-19);
+  });
+
+  it('should return a difference of -37 with input B6 and A#3', () => {
+    expect(pcDiff('B6', 'A#3')).to.eql(-37);
+    expect(pcDiff('A#3', 'B6')).to.eql(37);
   });
 
   it('should throw a TypeError PC is not in regular alpha set', () => {
