7b9be61b027c4c5222e4a300136853f61e1ae3b1
Ian McCunn <ianmccunn@gmail.com>
2017-04-09 11:31:30 -0700

    Adjust rounding for spnToFreq function.

--- test/spnToFreq.test.js
+++ test/spnToFreq.test.js
@@ -8,6 +8,7 @@ const spnToFreq = require('../lib/spnToFreq');
 
 describe('spnToFreq specification - ', () => {
   it('should return approximate freq 1864.66 for A#6', () => {
+    console.log(spnToFreq('A#6'))
     assert(util.fuzzyCompare(spnToFreq('A#6'), 1864.66));
   });
 
